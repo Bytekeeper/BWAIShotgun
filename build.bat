f656fab4ed7be568150e7e368d77eecdb029c1d7
Dennis Waldherr <bytekeeper+github@mailbox.org>
2022-04-25 22:19:51 +0200

    Added tm folder to zip

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 cargo build --release
-releng\7za a -pshotgun  -- bwaishotgun.7z target/release/bwaishotgun.exe bots tools SNP_DirectIP.snp game.toml shotgun.toml
+releng\7za a -pshotgun  -- bwaishotgun.7z target/release/bwaishotgun.exe bots tools tm SNP_DirectIP.snp game.toml shotgun.toml
 releng\7za rn -pshotgun -- bwaishotgun.7z target/release/bwaishotgun.exe BWAIShotgun.exe
